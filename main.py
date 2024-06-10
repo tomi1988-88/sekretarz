5a8b5b18b1bb80bf2b6c20628312ae85c75b5275
tomi1988-88 <62662726+tomi1988-88@users.noreply.github.com>
2024-06-10 15:46:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,12 @@
-import copy
-import tkinter as tk
-import customtkinter as ctk
-import os
-import re
-import pathlib
-import json
 import gc
-import shutil
-import uuid
-import webbrowser
-from tkinter import ttk
-import datetime as dt
-from tkinter import filedialog, messagebox
-from __sekretarz_lang import LANG
-from base_classes import (MyFrame,
-                          MyLabel,
-                          MyButton,
-                          MyEntry,
-                          MyListbox,
-                          MyTopLevel,
-                          MyMenu,
-                          MyScrollableFrame,
-                          MyRadiobutton,
-                          MyDialogWindow_AskDir,
-                          MyDialogWindow_AskDir_CreateDir,
-                          MyInputDialog)
-from panels import (TreePan,
-                    ZoomPan,
-                    DetailPan,
-                    RotatingPan,
-                    FileHistoryPan,
-                    ProjectHistoryPan)
-from typing import (List,
-                    Dict,)
+import customtkinter as ctk
+from lang_module import LANG
 from my_logging_module import (log_exception_decorator,
                                log_exception,
                                my_logger)
 from the_brain_module import TheBrain
 from menu_bar_module import MenuBar
 from views_module import MainMenuView
-
-class TempLayer:
-    def __init__(self, *args, **kwargs):
-        ... # todo: maybe it might work as a variable storage for the whole base view eg:
-        # temp_layer.curr_widget = base pro view which is not displayed in main window?
-        # class TempLayer(MyFrame)
-
-
-class LangVariables:
-    def __init__(self, *args, **kwargs):
-        ...
 
 
 @log_exception_decorator(log_exception)
@@ -61,8 +18,11 @@ class MainWindow(ctk.CTk):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.title(LANG.get("title"))
-        width = self.winfo_screenwidth()
-        height = self.winfo_screenheight()
+        # width = self.winfo_screenwidth()
+        # height = self.winfo_screenheight()
+        # self.geometry(f"{width}x{height}")
+        width = 1000
+        height = 600
         self.geometry(f"{width}x{height}")
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
